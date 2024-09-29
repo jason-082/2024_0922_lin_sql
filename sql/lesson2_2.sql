5967fa9a61fbf38195a0eb6c7e97d9f4b1f78939
jason <chari1234520001@gmail.com>
2024-09-29 15:43:25 +0800

    lesson2_6

--- sql/lesson2_2.sql
+++ sql/lesson2_2.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS hospital;
 
 CREATE TABLE IF NOT EXISTS hospital (
-    num SERIAL PRIMARY KEY,
-    live VARCHAR(5) NOT NULL,
-	hosp VARCHAR(30) NOT NULL UNIQUE,
-	tel VARCHAR(15),
-	telnum VARCHAR(7) ,
-	people VARCHAR(7),
+    id SERIAL PRIMARY KEY,
+    state VARCHAR(5) NOT NULL,
+	name VARCHAR(30) NOT NULL UNIQUE,
+	phone VARCHAR(15),
+	ext VARCHAR(7) ,
+	contact VARCHAR(7),
 	address VARCHAR(50) 
   	);
