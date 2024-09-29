fd3934f3c72d260a2f52a92cb11d5b94d73ba0ee
jason <chari1234520001@gmail.com>
2024-09-29 15:33:53 +0800

    hospital

--- sql/lesson2_2.sql
+++ sql/lesson2_2.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS hospital;
 
 CREATE TABLE IF NOT EXISTS hospital (
-    序號 SERIAL PRIMARY KEY,
-    直轄市或省轄縣市 VARCHAR(5) NOT NULL,
-	醫療機構名稱 VARCHAR(30) NOT NULL UNIQUE,
-	市話 VARCHAR(15),
-	分機 VARCHAR(7) ,
-	聯絡人 VARCHAR(7),
-	地址 VARCHAR(50) 
+    num SERIAL PRIMARY KEY,
+    live VARCHAR(5) NOT NULL,
+	hosp VARCHAR(30) NOT NULL UNIQUE,
+	tel VARCHAR(15),
+	telnum VARCHAR(7) ,
+	people VARCHAR(7),
+	address VARCHAR(50) 
   	);
