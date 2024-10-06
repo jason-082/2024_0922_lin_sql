4b783643a87af253c614f330e6cc71f7a6f7a494
林烱為 <chari1234520001@gmail.com>
2024-10-06 11:19:59 +0800

    lesson3_2

--- sql/lesson3_2.sql
+++ sql/lesson3_2.sql
@@ -1,1 +1,15 @@
-SELECT 
+SELECT code,name
+FROM stations;
+
+SELECT *
+FROM stations;
+
+SELECT code as 代碼
+FROM stations;
+
+SELECT code as 代碼, name as 車站名稱, 
+e_name as 英文名稱
+FROM stations;
+
+
+
