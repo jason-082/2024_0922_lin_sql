ff4938762e380d602e6a4d928e19aa14245d029f
林烱為 <chari1234520001@gmail.com>
2024-10-06 14:20:12 +0800

    modeify

--- sql/lesson3_3.sql
+++ sql/lesson3_3.sql
@@ -50,5 +50,14 @@ e_name as 英文名稱
 FROM stations
 WHERE id <= 20 AND id >= 10;
 
+SELECT id as 序號,code as 代碼, name as 車站名稱, 
+e_name as 英文名稱
+FROM stations
+WHERE id > 20 OR id < 10;
 
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, 
+e_name as 英文名稱
+FROM stations
+WHERE id NOT BETWEEN 10 AND 20;
 
