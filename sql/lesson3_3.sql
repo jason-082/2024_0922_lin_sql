5f43b38cbef4802bfee5ab04808e6a09658c2afa
林烱為 <chari1234520001@gmail.com>
2024-10-06 14:27:31 +0800

    modify

--- sql/lesson3_3.sql
+++ sql/lesson3_3.sql
@@ -61,3 +61,18 @@ e_name as 英文名稱
 FROM stations
 WHERE id NOT BETWEEN 10 AND 20;
 
+SELECT id as 序號,code as 代碼, name as 車站名稱, 
+e_name as 英文名稱
+FROM stations
+ORDER BY id DESC;
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, 
+e_name as 英文名稱
+FROM stations
+ORDER BY code DESC;
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, 
+e_name as 英文名稱
+FROM stations
+ORDER BY e_name;
+
